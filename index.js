1a9f5bc51317334c3d2f3d92d93750b5f1542b55
simon3000 <simon3000@163.com>
2019-09-27 07:20:32 +0200

    simple log receiver

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-const io = require('socket.io')
-io()
+const Server = require('socket.io')
+const io = new Server(9200, { serveClient: false })
+
+io.on('connect', socket => {
+  const { name } = socket.handshake.query
+  if (name) {
+    console.log(name, 'connected')
+  }
+})
