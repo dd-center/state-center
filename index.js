ddce883a8d3bcd3a8a745267436f96d5fd0d3bae
simon3000 <simon30002021@icloud.com>
2019-10-23 05:57:44 +0200

    join log

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const io = new Server(9200, { serveClient: false })
 io.on('connect', socket => {
   const { name } = socket.handshake.query
   if (name) {
+    const log = data => io.to('all').to('log').to(name).emit('log', { name, data })
+    socket.on('log', log)
+    socket.on('join', room => socket.join(room))
     console.log(name, 'connected')
   }
 })
