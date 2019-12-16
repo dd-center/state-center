27aabd0e0289209909f4bb7e7f8da26aafa236be
simon3000 <simon30002021@icloud.com>
2019-12-16 17:30:37 +0100

    unit test log, join, ask

--- api.js
+++ api.js
@@ -6,7 +6,6 @@ const { port } = require('./config')
 const baseURL = new URL('http://0.0.0.0')
 
 baseURL.port = port
-
 class CState extends EventEmitter {
   /**
    * @param { { name:string, group:string } | string } option
@@ -24,6 +23,8 @@ class CState extends EventEmitter {
       this.socket = io(url.href)
     }
 
+    this.stateTable = {}
+
     this.liveInterval = setInterval(() => {
       // console.log(233)
       if (this.connected) {
@@ -33,12 +34,39 @@ class CState extends EventEmitter {
 
     const socket = this.socket
     socket.on('connect', () => this.emit('connect'))
+    socket.on('state', async (key, ack) => {
+      if (typeof ack === 'function') {
+        ack(await this.getStateRoute(key)())
+      }
+    })
+    socket.on('log', (...params) => this.emit('log', ...params))
+    socket.on('error', (...params) => this.emit('error', ...params))
   }
+
+  stateRoute = stateTable => {
+    this.stateTable = { ...this.stateTable, ...stateTable }
+  }
+
+  getStateRoute = key => this.stateTable[key] || (() => undefined)
 
   close = () => {
     clearInterval(this.liveInterval)
     this.socket.close()
   }
+
+  /**
+   * @param { string } raw
+   * @param { object } extra
+   */
+  log = (raw, extra = {}) => this.socket.emit('log', { raw, ...extra })
+  /**
+   * @param { string } type
+   */
+  join = type => this.socket.emit('join', type)
+  /**
+   * @param { string } name
+   */
+  ask = name => key => new Promise(resolve => this.socket.emit('state', { name, key }, resolve))
 }
 
 module.exports = CState
--- test/test.js
+++ test/test.js
@@ -30,6 +30,36 @@ describe('State Center', function() {
       assert.isFalse(clients.has('test'))
     })
 
+    context('join()', function() {
+      runner.join('all')
+
+      it('log()', async function() {
+        const name = 'tester'
+        const cState = new CState({ name })
+        await wait(10)
+        const log = await new Promise(resolve => {
+          cState.log('nice')
+          runner.on('log', ({ name: n, data: { raw } }) => {
+            if (n === name) {
+              resolve(raw)
+            }
+          })
+        })
+        cState.close()
+        assert.strictEqual(log, 'nice')
+      })
+    })
+
+    it('ask()', async function() {
+      const name = 'answer'
+      const cState = new CState({ name })
+      cState.stateRoute({ testQ: () => Promise.resolve(233) })
+      await wait(10)
+      const result = await runner.ask(name)('testQ')
+      cState.close()
+      assert.strictEqual(result, 233)
+    })
+
     after(function() {
       runner.close()
     })
