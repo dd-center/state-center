a535cde616e3d9d4b3f883da4eaa62401cea80e2
Shigma <1700011071@pku.edu.cn>
2020-01-13 22:48:30 +0800

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   apps: [{
+    interpreter: './node_modules/.bin/ts-node',
+    interpreter_args: '-r tsconfig-paths/register',
     name: 'state center',
-    script: 'index.js',
+    script: 'src/index.ts',
     instances: 1,
     autorestart: true,
-    watch: false
-  }]
+    watch: false,
+  }],
 }
