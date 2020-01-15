a2360a58f005dcf64366e3df338827e48e14dafa
simon3000 <simon30002021@icloud.com>
2020-01-15 04:17:46 +0100

    Revert "update ecosystem"
    This reverts commit a535cde616e3d9d4b3f883da4eaa62401cea80e2.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,9 @@
 module.exports = {
   apps: [{
-    interpreter: './node_modules/.bin/ts-node',
-    interpreter_args: '-r tsconfig-paths/register',
     name: 'state center',
-    script: 'src/index.ts',
+    script: 'index.js',
     instances: 1,
     autorestart: true,
-    watch: false,
-  }],
+    watch: false
+  }]
 }
