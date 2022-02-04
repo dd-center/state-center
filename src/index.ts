2a945ce38829f835df715826e94c9f96f6c9379f
simon3000 <w@simon3k.moe>
2022-02-05 02:46:42 +0800

    print log

--- src/index.ts
+++ src/index.ts
@@ -46,6 +46,7 @@ io.on('connect', socket => {
 
   socket.on('log', data => {
     io.to('all').to('log').to(name).emit('log', { name, data })
+    console.log('log', { name }, data)
   })
 
   socket.on('join', room => socket.join(room))
